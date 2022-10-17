ece02a7d3f089d38c2b8b4c4eb49763faf24cd0f
CHunter <nekto@must-die.ru>
2022-10-17 08:33:22 +0300

    initial commit

---
+++ task1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(sorting("FFFFFFFFdddddddddVVVVVVVVVVoooooooooЯЯЯЯЯЯЯЯЯЯЯаааааААААА"))
+}
+
+func sorting(str string) (outStr string) {
+	m := make(map[string]int)
+	var charSlice []string
+	for _, r := range str {
+		char := string(r)
+		if _, ok := m[char]; !ok {
+			charSlice = append(charSlice, char)
+		}
+		m[char] += 1
+	}
+	sort.Strings(charSlice)
+	for _, v := range charSlice {
+		outStr += v + strconv.Itoa(m[v])
+	}
+	return
+}
